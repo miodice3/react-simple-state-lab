88486a1b1a939fe2022988ff91544d4a9406fa9e
Michael Iodice <miodice3@gmail.com>
2021-02-05 15:42:17 -0700

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+
+    // state = {color: ''}
+
+    constructor(props){
+        super()
+        this.state = {
+            color: props.value
+        }
+    }
+
+    handleSubmit = () => {
+        this.setState(
+            {color: '#333'}
+        )
+    }
+
+    render(){
+        return (
+            <div    
+                
+                className="cell"
+                style={{backgroundColor: this.state.color}}
+                onClick={this.handleSubmit}
+                >
+
+            </div>
+        )
+    }
+
+}
